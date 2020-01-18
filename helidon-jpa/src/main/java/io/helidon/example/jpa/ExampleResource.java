25bf4b3346c6decfd1fde79122eb61fb5a0f8720
Emerson Castaneda <emecas@users.sourceforge.net>
2020-01-18 11:52:55 -0500

    Inject a Container-Managed EntityManager

--- helidon-jpa/src/main/java/io/helidon/example/jpa/ExampleResource.java
+++ helidon-jpa/src/main/java/io/helidon/example/jpa/ExampleResource.java
@@ -4,11 +4,19 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
+import javax.enterprise.context.Dependent;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 /**
  * Simple JAXRS resource class.
  */
 @Path("/example")
+@Dependent
 public class ExampleResource {
+
+    @PersistenceContext 
+    private EntityManager em;
 
     @Produces("text/plain")
     @Path("/")
